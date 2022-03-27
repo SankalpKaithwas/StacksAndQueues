4cda1344b54d9a53e41f3e8ffc7ed1f7e9fa356b
unknown <kaithwas.697@gmail.com>
2022-03-27 17:44:27 +0530

    Created stack using LinkedList

---
+++ StackAndQueue/StackAndQueue/Stack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StackAndQueue
+{
+    internal class Stack
+    {
+        LinkedList linkedList;
+        public Stack()
+        {
+            linkedList = new LinkedList();
+        }
+        public void Push(int data)
+        {
+            linkedList.AddNode(data);
+        }
+        public void DisplayStack()
+        {
+            Console.WriteLine("Stack elements are");
+            linkedList.Display();
+        }
+    }
+}
